561b1fc95fadb002b16107329bc45f6984881830
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-04 16:35:54 +0100

    Algorithm and tests

--- project-euler-31/src/main/java/pl/grzeslowski/project_euler_31/ProjectEuler31NsnMoneyChange.java
+++ project-euler-31/src/main/java/pl/grzeslowski/project_euler_31/ProjectEuler31NsnMoneyChange.java
@@ -2,9 +2,33 @@ package pl.grzeslowski.project_euler_31;
 
 public class ProjectEuler31NsnMoneyChange implements ProjectEuler31 {
 
+    private final CoinFactory coinFactory = new CoinFactory();
+
+    @Override
     public long waysToChangeCoin(final int coin) {
-        // TODO Auto-generated method stub
-        return 0;
+        return waysToChangeCoin(coin, coinFactory.getNextCoin(coin));
+    }
+
+    private long waysToChangeCoin(final int coin, final int coinToUse) {
+        if (coinToUse == 1) {
+            return 1L;
+        } else if (coin == 0) {
+            return 1L;
+        } else if (coin < 0) {
+            return 0;
+        } else if (coinToUse == 0) {
+            return 0L;
+        } else {
+
+            // left side uses coinToUse
+            final long left = waysToChangeCoin(coin - coinToUse, coinToUse);
+
+            // right side does not use cointToUse
+            final long right = waysToChangeCoin(coin,
+                    coinFactory.getNextCoin(coinToUse));
+
+            return left + right;
+        }
     }
 
 }
--- project-euler-31/src/test/java/pl/grzeslowski/project_euler_31/ProjectEuler31NsnMoneyChangeTest.java
+++ project-euler-31/src/test/java/pl/grzeslowski/project_euler_31/ProjectEuler31NsnMoneyChangeTest.java
@@ -3,6 +3,7 @@ package pl.grzeslowski.project_euler_31;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.fest.assertions.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -10,20 +11,18 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(value = Parameterized.class)
 public class ProjectEuler31NsnMoneyChangeTest {
-    private final long in;
+    private final int in;
     private final long out;
 
-    public ProjectEuler31NsnMoneyChangeTest(final long in, final long out) {
+    public ProjectEuler31NsnMoneyChangeTest(final int in, final long out) {
         this.in = in;
         this.out = out;
     }
 
     @Parameters
     public static Collection<Object[]> data() {
-        final Object[][] data = new Object[][] { { 1, 1L }, { 3, 8L },
-                { 6, 9L }, { 9, 20L }, { 8, 17L }, { 10, 20L }, { 11, 20L },
-                { 12, 20L }, { 13, 20L }, { 14, 20L }, { 15, 20L },
-                { 16, 20L }, { 17, 20L }, { 18, 21L } };
+        final Object[][] data = new Object[][] { { 1, 0 }, { 2, 1 },
+                { 5, 1 + 1 + 1 } };
         return Arrays.asList(data);
     }
 
@@ -31,10 +30,12 @@ public class ProjectEuler31NsnMoneyChangeTest {
     public void test_normal_parameters() {
 
         // given
+        final ProjectEuler31NsnMoneyChange euler = new ProjectEuler31NsnMoneyChange();
 
         // when
+        final long waysToChangeCoin = euler.waysToChangeCoin(in);
 
         // then
-//        Assertions.assertThat().isEqualTo(out);
+        Assertions.assertThat(waysToChangeCoin).isEqualTo(out);
     }
 }
